6f06a0612ad95b787a120cce3dc414d174814d9e
Abdenaser <a.aroukhsiss_etu@enset-media.ac.ma>
2022-12-25 18:56:59 +0100

    commit

---
+++ front-end-consul/src/Components/footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+Box,
+} from "./footerStyles";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "white",
+				textAlign: "center",
+				marginTop: "0px",
+                fontSize: "18px" }}>
+		Lahcen Lamkirich &copy; All rights reserved 2022
+	</h1>
+	</Box>
+);
+};
+export default Footer;
